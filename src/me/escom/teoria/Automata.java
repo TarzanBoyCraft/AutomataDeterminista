7810fa9c81635171665bfbe606170c1e1ed7a572
Servín <tarzanboycraft@hotmail.com>
2019-03-25 11:36:40 -0600

    Add files via upload

--- src/me/escom/teoria/Automata.java
+++ src/me/escom/teoria/Automata.java
@@ -5,26 +5,46 @@ import java.util.ArrayList;
 public class Automata {
 
 	private ArrayList<Estado> estados;
+	private ArrayList<Integer> estados_finales;
 	
 	public Automata(){
 		estados = new ArrayList<Estado>();
+		estados_finales = new ArrayList<Integer>();
 	}
 	
-	public boolean probar(String cadena, boolean completa){
+	public boolean probar(String cadena){
 		if(cadena == null)
 			return false;
 		if(cadena.length() == 0)
 			return false;
-		if(completa){
-			int actual = 0;
-			while)=
+		
+		int actual = 0;
+		int num_estado = 0;
+		while(actual < cadena.length()) {
+			num_estado = estados.get(num_estado).test(cadena.charAt(actual));
+			if(num_estado == -1)
+				return false;
+			actual++;
 		}
-		boolean pasa = false;
-		while()
+		
+		//Probar si quedó en estado final
+		for(int i = 0; i < estados_finales.size(); i++) {
+			if(num_estado == estados_finales.get(i))
+				return true;
+		}
+		
 		return false;
 	}
 	
-	public void agregarEstado(Estado estado){
+	public void addEstado(Estado estado){
 		estados.add(estado);
+	}
+	
+	public Estado getEstado(int indice) {
+		return estados.get(indice);
+	}
+	
+	public void agregarEstadoFinal(int estado_final) {
+		estados_finales.add(estado_final);
 	}
 }
--- src/me/escom/teoria/Principal.java
+++ src/me/escom/teoria/Principal.java
@@ -3,7 +3,125 @@ package me.escom.teoria;
 public class Principal {
 	
 	public static void main(String [] args){
-		System.out.println("hola");
+		System.out.println("INICIO");
+		Estado estado0 = (char a)->{
+			if(a == '0')
+				return 1;
+			if(a == '+' || a == '-')
+				return 7;
+			if(a == '1' || a == '2' || a == '3' || a == '4' || a == '5' || a == '6' || a == '7' || a == '8' || a == '9')
+				return 8;
+			return -1;
+		};
+		
+		Estado estado1 = (char a)->{
+			if(a == '.')
+				return 2;
+			return -1;
+		};
+		
+		Estado estado2 = (char a)->{
+			if(a == '0' || a == '1' || a == '2' || a == '3' || a == '4' || a == '5' ||a == '6' ||a == '7' || a == '8' || a == '9')
+				return 3;
+			return -1;
+		};
+		
+		Estado estado3 = (char a)->{
+			if(a == '0' || a == '1' || a == '2' || a == '3' || a == '4' || a == '5' ||a == '6' ||a == '7' || a == '8' || a == '9')
+				return 3;
+			if(a == 'e' || a == 'E')
+				return 4;
+			return -1;
+		};
+		
+		Estado estado4 = (char a)->{
+			if(a == '+' || a == '-')
+				return 5;
+			return -1;
+		};
+		
+		Estado estado5 = (char a)->{
+			if(a == '1' || a == '2' || a == '3' || a == '4' || a == '5' || a == '6' || a == '7' || a == '8' || a == '9')
+				return 6;
+			return -1;
+		};
+		
+		Estado estado6 = (char a)->{
+			if(a == '0' || a == '1' || a == '2' || a == '3' || a == '4' || a == '5' ||a == '6' ||a == '7' || a == '8' || a == '9')
+				return 6;
+			return -1;
+		};
+		Estado estado7 = (char a)->{
+			if(a == '1' || a == '2' || a == '3' || a == '4' || a == '5' || a == '6' || a == '7' || a == '8' || a == '9')
+				return 8;
+			if(a == '0')
+				return 9;
+			return -1;
+		};
+		
+		Estado estado8 = (char a)->{
+			if(a == '0' || a == '1' || a == '2' || a == '3' || a == '4' || a == '5' ||a == '6' ||a == '7' || a == '8' || a == '9')
+				return 8;
+			if(a == '.')
+				return 2;
+			return -1;
+		};
+		
+		Estado estado9 = (char a)->{
+			if(a == '.')
+				return 10;
+			return -1;
+		};
+		
+		Estado estado10 = (char a)->{
+			if(a == '0' || a == '1' || a == '2' || a == '3' || a == '4' || a == '5' ||a == '6' ||a == '7' || a == '8' || a == '9')
+				return 11;
+			return -1;
+		};
+		
+		Estado estado11 = (char a)->{
+			if(a == '1' || a == '2' || a == '3' || a == '4' || a == '5' || a == '6' || a == '7' || a == '8' || a == '9')
+				return 12;
+			return -1;
+		};
+		
+		Estado estado12 = (char a)->{
+			if(a == '0' || a == '1' || a == '2' || a == '3' || a == '4' || a == '5' ||a == '6' ||a == '7' || a == '8' || a == '9')
+				return 12;
+			if(a == 'e' || a == 'E')
+				return 4;
+			return -1;
+		};
+		
+		Automata automata = new Automata();
+		
+		automata.addEstado(estado0);
+		automata.addEstado(estado1);
+		automata.addEstado(estado2);
+		automata.addEstado(estado3);
+		automata.addEstado(estado4);
+		automata.addEstado(estado5);
+		automata.addEstado(estado6);
+		automata.addEstado(estado7);
+		automata.addEstado(estado8);
+		automata.addEstado(estado9);
+		automata.addEstado(estado10);
+		automata.addEstado(estado11);
+		automata.addEstado(estado12);
+		
+		automata.agregarEstadoFinal(3);
+		automata.agregarEstadoFinal(6);
+		automata.agregarEstadoFinal(8);
+		automata.agregarEstadoFinal(12);
+		String cadenita = "0.0E+0.0";
+		System.out.println("Cadena a probar:" + cadenita);
+		boolean pasa = automata.probar(cadenita);
+		if(pasa) {
+			System.out.println("Es real");			
+		}else {
+			System.out.println("No es real");
+		}
+		System.out.println("FIN");
 	}
 
 }
